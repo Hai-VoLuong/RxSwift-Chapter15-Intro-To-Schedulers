cf30111af7598827a0df2b75b3c80d498ddd0d17
HaiVL <hai.softech@gmail.com>
2017-12-15 17:12:41 +0700

    ...

--- Schedulers/main.swift
+++ Schedulers/main.swift
@@ -33,3 +33,35 @@ animal
   .dump()
   .dumpingSubscription()
   .addDisposableTo(bag)
+
+let fruit = Observable<String>.create { observer in
+    observer.onNext("[apple]")
+    sleep(2)
+    observer.onNext("[pineapple]")
+    sleep(2)
+    observer.onNext("strawberry")
+    return Disposables.create()
+}
+
+let animalsThread = Thread() {
+    sleep(3)
+    animal.onNext("[cat]")
+    sleep(3)
+    animal.onNext("[tiger]")
+    sleep(3)
+    animal.onNext("[fox]")
+    sleep(3)
+    animal.onNext("leopard")
+}
+
+animalsThread.name = "Animals Thread"
+animalsThread.start()
+
+fruit
+.subscribeOn(globalScheduler)
+.dump()
+.observeOn(MainScheduler.instance)
+.dumpingSubscription()
+.addDisposableTo(bag)
+
+RunLoop.main.run(until: Date(timeIntervalSinceNow: 13))
